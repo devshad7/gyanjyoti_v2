1e536d7fbbaa7f3532b6a3037b1683bf67713c86
Md Shad Khan <devshaad@Mac.lan>
2025-04-20 21:37:28 +0530

    [FIXED]: some styling

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import Login from "@/components/layout/auth/Login";
+import Navbar from "@/components/layout/Navbar";
+import React from "react";
+
+function Page() {
+  return (
+    <>
+      <Navbar />
+      <Login />
+    </>
+  );
+}
+
+export default Page;
