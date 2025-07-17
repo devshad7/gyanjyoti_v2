cd9a766252f4fa46648c854d772ec6ff1322fa70
codewithbirat1 <biratpandey9@gmail.com>
2025-07-17 22:42:56 +0545

    [UPDATE]: Error fixed

--- src/lib/cloudinary-notes-service.ts
+++ src/lib/cloudinary-notes-service.ts
@@ -2,7 +2,7 @@ import { v2 as cloudinary, UploadApiResponse } from 'cloudinary'
 
 // Check for required environment variables
 const requiredEnvVars = {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 }
